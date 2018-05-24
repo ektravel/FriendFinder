8642c1f3b7880226c5823b55702d1bf797786c18
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-23 23:38:55 -0500

    adding JS files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,19 @@
-// Your apiRoutes.js file should contain two routes:
-// A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+var friends = require("../data/friends");
+
+module.exports = function(app){
+    // A GET route with the url /api/friends used to display a JSON of all friends
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+    // A POST routes /api/friends used to handle incoming survey results
+    app.post("/api/friends", function(req, res){
+        for (i = 0; i < friends.length; i++){
+            friends.push(req.body);
+            console.log("New user added!");
+            // res.json(true);
+        };
+    });
+};
+
+
+
