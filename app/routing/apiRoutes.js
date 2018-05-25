6e30591262842805df30160bc66ffd04c7d28e59
ektravel <38017980+ektravel@users.noreply.github.com>
2018-05-24 21:18:46 -0500

    added logic for friends.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,11 +7,25 @@ module.exports = function(app){
     });
     // A POST routes /api/friends used to handle incoming survey results
     app.post("/api/friends", function(req, res){
-        for (i = 0; i < friends.length; i++){
-            friends.push(req.body);
-            console.log("New user added!");
-            res.json(false);
-        };
+        var newUserSurvey = req.body;
+        console.log(newUserSurvey.name);
+        console.log(newUserSurvey.photo);
+        console.log(newUserSurvey.scores[0]);
+        console.log(newUserSurvey.scores[1]);
+        console.log(newUserSurvey.scores[2]);
+        console.log(newUserSurvey.scores[3]);
+        console.log(newUserSurvey.scores[4]);
+        console.log(newUserSurvey.scores[5]);
+        console.log(newUserSurvey.scores[6]);
+        console.log(newUserSurvey.scores[7]);    
+        console.log(newUserSurvey.scores[8]);
+        console.log(newUserSurvey.scores[9]);
+
+        console.log(newUserSurvey);
+
+        friends.push(newUserSurvey);
+        res.json(newUserSurvey);
+        
     });
 };
 
